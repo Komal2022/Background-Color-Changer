bd03bf3d831f10e05d48b4a53b8dc89394ed4229
shoaib inamdar <shoaibinamdar14@gmail.com>
2023-10-26 20:48:09 +0530

    new changes

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-var colo = [ "red" ,"black" ,"green" , "yellow" , "maroon" ,"orange" , "blue", "#5d0ae2","pink"];
+var colo = [ "#4deeea" ,"#74ee15" ,"#ffe700" , "#f000ff" , "#001eff" ,"orange" , "blue", "#5d0ae2","pink"];
 var i=0;
 
 var div_arr = document.getElementsByClassName("divt");
 var ab =0;
-console.log(ab);
-
-//var but_arr = document.getElementsByClassName("buttons");
-//hello world!
 
 function func1()
 {
@@ -17,42 +13,4 @@ function func1()
     i=0;
     if(ab==3)
     ab=0;
-    console.log('i',i);
-    console.log('ab',ab);
 }
-
-
-
-
-// function func2()
-// {
-//     document.getElementById("div2").style.backgroundColor="black";
-// }
-// function func3()
-// {
-//     document.getElementById("div3").style.backgroundColor="green";
-// }
-// function func4()
-// {
-//     document.getElementById("div1").style.backgroundColor="yellow";
-// }
-// function func5()
-// {
-//     document.getElementById("div2").style.backgroundColor="maroon";
-// }
-// function func6()
-// {
-//     document.getElementById("div3").style.backgroundColor="orange";
-// }
-// function func7()
-// {
-//     document.getElementById("div1").style.backgroundColor="blue";
-// }
-// function func8()
-// {
-//     document.getElementById("div2").style.backgroundColor="#5d0ae2";
-// }
-// function func9()
-// {
-//     document.getElementById("div3").style.backgroundColor="pink";
-// }
